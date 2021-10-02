e3ba8ca69e3225e18533eea66aa7711af7751deb
Motorina Arina <arinamotorinaaaaa@gmail.com>
2021-10-02 13:46:28 +0300

    Initial Commit

---
+++ task 8/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  task 8
+//
+//  Created by ПК on 02.10.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
